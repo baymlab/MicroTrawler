c5649bf87f655eb161031bbc408dd324986c478a
luak <arya_kaul@g.harvard.edu>
2020-05-07 11:57:33 -0700

    may 7

---
+++ helper_scripts/nctc/extract_date.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import re
+import sys
+# heuristic, first look in 'Isolated From', then look in 'History', then look in 'Data', then look in 'Other', then look in 'Accession Date', then look in 'Authority', then look in 'Bibliography', then say it's not here ya lil shit
+x = pd.read_csv(sys.argv[1], sep='\t', index_col=0)
+for index,row in x.iterrows():
+    year_ext = None
+    # super sexy regex to find year
+    regex_for_year = "(?:19|20)\d{2}"
+    i_f = re.findall(regex_for_year, str(row['Isolated_From']))
+    if len(i_f)!=0:
+        year_ext = min(i_f)
+    hist = re.findall(regex_for_year, str(row['History']))
+    if len(hist)!=0 and year_ext==None:
+        year_ext=min(hist)
+    data = re.findall(regex_for_year, str(row['Data']))
+    if len(data)!=0 and year_ext==None:
+        year_ext=min(data)
+    other = re.findall(regex_for_year, str(row['Other']))
+    if len(other)!=0 and year_ext==None:
+        year_ext=min(other)
+    acc_date = re.findall(regex_for_year, str(row['Accession_Date']))
+    if len(acc_date)!=0 and year_ext==None:
+        year_ext=min(acc_date)
+    auth = re.findall(regex_for_year, str(row['Authority']))
+    if len(auth)!=0 and year_ext==None:
+        year_ext=min(auth)
+    bib = re.findall(regex_for_year, str(row['Bibliography']))
+    if len(bib)!=0 and year_ext==None:
+        year_ext=min(bib)
+    if year_ext == None:
+        print(row['NCTC_Number']+"\t"+"-1")
+    else:
+        print(row['NCTC_Number']+"\t"+year_ext)
--- helper_scripts/nctc/trawler.py
+++ helper_scripts/nctc/trawler.py
@@ -5,9 +5,15 @@ import os.path
 
 in_nctc = sys.argv[1]
 out_db = sys.argv[2]
+
+# read html table
 x = pd.read_html(in_nctc,index_col=0)
+
+# nothing here yet, leave
 if len(x) == 0:
     sys.exit(2)
+
+# get it all purty
 full_info = x[0].T
 better_col = {}
 for ugly_col in full_info:
@@ -15,12 +21,18 @@ for ugly_col in full_info:
     sexy_col = sexy_col.replace(" ", "_")
     better_col[ugly_col] = sexy_col
 full_info = full_info.rename(columns=better_col)
+
+# look for extended bib table and slap it in
 try:
     bib = "--".join(x[1].index.tolist())
     full_info["Extended_Bibliography"] = bib
 except:
     pass
+
+# append output if file already exists
 if os.path.isfile(out_db):
     prev_db = pd.read_csv(out_db, sep='\t', index_col=0, dtype=str)
     full_info = prev_db.merge(full_info, how="outer")
+
+# dump output to file
 full_info.to_csv(out_db, sep='\t')
