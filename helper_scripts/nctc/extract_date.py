9698eaec44c051d96e86ab480edfd73786874bd7
luak <arya_kaul@g.harvard.edu>
2020-05-15 12:50:30 -0700

    fixed bug with date extractor for nctc

--- helper_scripts/nctc/extract_date.py
+++ helper_scripts/nctc/extract_date.py
@@ -10,24 +10,31 @@ for index,row in x.iterrows():
     i_f = re.findall(regex_for_year, str(row['Isolated_From']))
     if len(i_f)!=0:
         year_ext = min(i_f)
+        if int(year_ext) > 2020: year_ext = None
     hist = re.findall(regex_for_year, str(row['History']))
     if len(hist)!=0 and year_ext==None:
         year_ext=min(hist)
+        if int(year_ext) > 2020: year_ext = None
     data = re.findall(regex_for_year, str(row['Data']))
     if len(data)!=0 and year_ext==None:
         year_ext=min(data)
+        if int(year_ext) > 2020: year_ext = None
     other = re.findall(regex_for_year, str(row['Other']))
     if len(other)!=0 and year_ext==None:
         year_ext=min(other)
-    acc_date = re.findall(regex_for_year, str(row['Accession_Date']))
-    if len(acc_date)!=0 and year_ext==None:
-        year_ext=min(acc_date)
+        if int(year_ext) > 2020: year_ext = None
     auth = re.findall(regex_for_year, str(row['Authority']))
     if len(auth)!=0 and year_ext==None:
         year_ext=min(auth)
+        if int(year_ext) > 2020: year_ext = None
+    acc_date = re.findall(regex_for_year, str(row['Accession_Date']))
+    if len(acc_date)!=0 and year_ext==None:
+        year_ext=min(acc_date)
+        if int(year_ext) > 2020: year_ext = None
     bib = re.findall(regex_for_year, str(row['Bibliography']))
     if len(bib)!=0 and year_ext==None:
         year_ext=min(bib)
+        if int(year_ext) > 2020: year_ext = None
     if year_ext == None:
         print(row['NCTC_Number']+"\t"+"-1")
     else:
